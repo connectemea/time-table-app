7c260885c6dfe91a4e8bd1a2c283ce8de30588f5
afeef <afeef@sanoft.com>
2021-09-02 19:55:00 +0530

    header component added

--- src/component/pages/TimeTable/TimeTable.js
+++ src/component/pages/TimeTable/TimeTable.js
@@ -1,8 +1,14 @@
-import styles from './Style.module.css';
+import styles from './styles.module.css';
+import Header from './Header';
+
 export default function TimeTable (){
+    
     return(
+        <>
+        <Header/>
         <div className={styles.contentWrapper}>
             Time Table page 
         </div>
+        </>
     )
 }
