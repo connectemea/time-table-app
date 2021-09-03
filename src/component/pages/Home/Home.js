ab6eb4c0d312bcdf291f7a239f18d474d0f2ede8
dilshad <dilshadbasith01@gmail.com>
2021-09-03 22:08:00 +0530

    bottom line

--- src/component/pages/Home/Home.js
+++ src/component/pages/Home/Home.js
@@ -6,7 +6,12 @@ const Home = () => {
       <div className={styles.titleSection}>
         <div className={styles.mainTitle}>TIME TABLE</div>
         <div className={styles.subTitle}>EMEA COLLEGE KONDOTTY</div>
+      </div>     
+      <div className={styles.bottomTitle}>Crafted with&#128150;
+      <span className={styles.tinker}>Tinker</span>
+      <span className={styles.hub}>Hub</span>
       </div>
+      
     </div>
   );
 };
