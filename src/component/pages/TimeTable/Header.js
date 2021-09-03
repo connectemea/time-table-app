c788b09fd7b16d92d45c419a2666a07363445c5a
aseelkp <aseelaroor10@gmail.com>
2021-09-03 23:37:18 +0530

    imgs

--- src/component/pages/TimeTable/Header.js
+++ src/component/pages/TimeTable/Header.js
@@ -4,10 +4,7 @@ export default function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img
-          src="https://st2.depositphotos.com/6831718/10871/v/950/depositphotos_108712214-stock-illustration-clock-icon-clock-symbol-clock.jpg"
-          alt="logo"
-        />
+        <img src="" alt="" />
       </div>
       <div className={styles.title}>
         <p className={styles.date}>28 Aug 2021 </p>
