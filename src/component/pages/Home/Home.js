c16128c9e7a70a6f8a518ab1a5675ee9d33a5a53
aseelkp <aseelaroor10@gmail.com>
2021-09-05 11:52:06 +0530

    loginBox

--- src/component/pages/Home/Home.js
+++ src/component/pages/Home/Home.js
@@ -8,38 +8,45 @@ const Home = () => {
         <div className={styles.mainTitle}>TIME TABLE</div>
         <div className={styles.subTitle}>EMEA COLLEGE KONDOTTY</div>
       </div>
-      <div>
-        <select className={styles.selectBox1} id="dropdown">
-          <option value="Select Department">Select Department</option>
-          <option value="BSc Computer Science">BSc Computer Science</option>
-          <option value="BA Economics">BA Economics</option>
-          <option value="B.Com Cop">B.Com</option>
-          <option value="BA English">BA English</option>
-          <option value="BSc Microbiology">BSc Microbiology</option>
-          <option value="BA West Asian Studies">BA West Asian Studies</option>
-          <option value="BSc Biotechnology">BSc Biotechnology</option>
-          <option value="BSc Biochemistry">BSc Biochemistry</option>
-          <option value="BBA">BBA</option>
-          <option value="B.Com Computer Application">
-            B.Com Computer Application
-          </option>
-          <option value="BVoc Logistics">BVoc Logistics</option>
-          <option value="BVoc Accounting and Taxation">
-            BVoc Accounting and Taxation
-          </option>
-        </select>
+      <div className={styles.MiddleContent}>
+        <div className={styles.LoginBox}>
+          <div>
+            <select className={styles.selectBox1} id="dropdown">
+              <option value="Select Department">Select Department</option>
+              <option value="BSc Computer Science">BSc Computer Science</option>
+              <option value="BA Economics">BA Economics</option>
+              <option value="B.Com Cop">B.Com</option>
+              <option value="BA English">BA English</option>
+              <option value="BSc Microbiology">BSc Microbiology</option>
+              <option value="BA West Asian Studies">
+                BA West Asian Studies
+              </option>
+              <option value="BSc Biotechnology">BSc Biotechnology</option>
+              <option value="BSc Biochemistry">BSc Biochemistry</option>
+              <option value="BBA">BBA</option>
+              <option value="B.Com Computer Application">
+                B.Com Computer Application
+              </option>
+              <option value="BVoc Logistics">BVoc Logistics</option>
+              <option value="BVoc Accounting and Taxation">
+                BVoc Accounting and Taxation
+              </option>
+            </select>
+          </div>
+          <div>
+            <select className={styles.selectBox2} id="dropdown">
+              <option value="Select Semester">Select Semester</option>
+              <option value="1st Sem">1st Sem</option>
+              <option value="2nd Sem">2nd Sem</option>
+              <option value="3rd Sem">3rd Sem</option>
+            </select>
+          </div>
+          <div>
+            <button className={styles.button}>Show Timetable</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <select className={styles.selectBox2} id="dropdown">
-          <option value="Select Semester">Select Semester</option>
-          <option value="1st Sem">1st Sem</option>
-          <option value="2nd Sem">2nd Sem</option>
-          <option value="3rd Sem">3rd Sem</option>
-        </select>
-      </div>
-      <div>
-        <button className={styles.button}>Show Timetable</button>
-      </div>
+
       <div className={styles.bottomTitle}>
         Crafted with&#128150;
         <img className={styles.brandName} src={BrandName} />
