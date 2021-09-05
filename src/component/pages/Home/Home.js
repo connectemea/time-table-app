c80fbf18804b5085e2b49238386b0d4f89726b47
afeef <afeef@sanoft.com>
2021-09-05 13:02:37 +0530

    design issues fixed

--- src/component/pages/Home/Home.js
+++ src/component/pages/Home/Home.js
@@ -10,38 +10,38 @@ const Home = () => {
       </div>
       <div className={styles.MiddleContent}>
         <div className={styles.LoginBox}>
-          <div>
-            <select className={styles.selectBox1} id="dropdown">
-              <option value="Select Department">Select Department</option>
-              <option value="BSc Computer Science">BSc Computer Science</option>
-              <option value="BA Economics">BA Economics</option>
-              <option value="B.Com Cop">B.Com</option>
-              <option value="BA English">BA English</option>
-              <option value="BSc Microbiology">BSc Microbiology</option>
-              <option value="BA West Asian Studies">
+          <div className={styles.departmentBox}>
+            <select className={styles.selectBox} id="dropdown">
+              <option className={styles.selectOption} value="Select Department">Select Department</option>
+              <option className={styles.selectOption} value="BSc Computer Science">BSc Computer Science</option>
+              <option className={styles.selectOption} value="BA Economics">BA Economics</option>
+              <option className={styles.selectOption} value="B.Com Cop">B.Com</option>
+              <option className={styles.selectOption} value="BA English">BA English</option>
+              <option className={styles.selectOption} value="BSc Microbiology">BSc Microbiology</option>
+              <option className={styles.selectOption} value="BA West Asian Studies">
                 BA West Asian Studies
               </option>
-              <option value="BSc Biotechnology">BSc Biotechnology</option>
-              <option value="BSc Biochemistry">BSc Biochemistry</option>
-              <option value="BBA">BBA</option>
-              <option value="B.Com Computer Application">
+              <option className={styles.selectOption} value="BSc Biotechnology">BSc Biotechnology</option>
+              <option className={styles.selectOption} value="BSc Biochemistry">BSc Biochemistry</option>
+              <option className={styles.selectOption} value="BBA">BBA</option>
+              <option className={styles.selectOption} value="B.Com Computer Application">
                 B.Com Computer Application
               </option>
-              <option value="BVoc Logistics">BVoc Logistics</option>
-              <option value="BVoc Accounting and Taxation">
+              <option className={styles.selectOption} value="BVoc Logistics">BVoc Logistics</option>
+              <option className={styles.selectOption} value="BVoc Accounting and Taxation">
                 BVoc Accounting and Taxation
               </option>
             </select>
           </div>
-          <div>
-            <select className={styles.selectBox2} id="dropdown">
-              <option value="Select Semester">Select Semester</option>
-              <option value="1st Sem">1st Sem</option>
-              <option value="2nd Sem">2nd Sem</option>
-              <option value="3rd Sem">3rd Sem</option>
+          <div className={styles.semesterBox}>
+            <select className={styles.selectBox} id="dropdown">
+              <option className={styles.selectOption} value="Select Semester">Select Semester</option>
+              <option className={styles.selectOption} value="1st Sem">1st Sem</option>
+              <option className={styles.selectOption} value="2nd Sem">2nd Sem</option>
+              <option className={styles.selectOption} value="3rd Sem">3rd Sem</option>
             </select>
           </div>
-          <div>
+          <div className={styles.showBtn}>
             <button className={styles.button}>Show Timetable</button>
           </div>
         </div>
