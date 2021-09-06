d2d1f079a7129b01cb17f14514d0697b4dcd6563
afeef <afeef@sanoft.com>
2021-09-06 13:59:49 +0530

    week timeTable structure is added

--- src/component/pages/TimeTable/TimeTable.js
+++ src/component/pages/TimeTable/TimeTable.js
@@ -1,18 +1,150 @@
-import styles from './styles.module.css';
-import Header from './Header';
-import Days from './Days';
-import DayTimeLine from './DayTimeLine';
-import TimeTableOfDay from './TimeTableOfDay';
-export default function TimeTable (){
-    return(
-        <>
-        
-        <div className={styles.contentWrapper}>
-            <Header/>
-            <Days/>
-            <DayTimeLine/>
-            <TimeTableOfDay/>
-        </div>
-        </>
-    )
+import styles from "./styles.module.css";
+import Header from "./Header";
+import Days from "./Days";
+import DayTimeLine from "./DayTimeLine";
+import TimeTableOfDay from "./TimeTableOfDay";
+export default function TimeTable() {
+    const curenntDay = new Date()
+  const timeTable = [
+    [
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+    ],
+    [
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+    ],
+    [
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+    ],
+    [
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+    ],
+    [
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+      {
+        sub: "maths",
+        time: "9-10",
+      },
+    ]
+  ];
+  return (
+    <>
+      <div className={styles.contentWrapper}>
+        <Header />
+        <Days />
+        <DayTimeLine />
+        <TimeTableOfDay />
+      </div>
+    </>
+  );
 }
