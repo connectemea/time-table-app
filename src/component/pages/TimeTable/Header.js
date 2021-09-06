afdea0ea160fecc3ed9dfbdda4daa1487146fe13
aseelkp <aseelaroor10@gmail.com>
2021-09-06 16:07:20 +0530

    Date

--- src/component/pages/TimeTable/Header.js
+++ src/component/pages/TimeTable/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Logo from '../images/logo.png';
-export default function Header() {
+export default function Header({today}) {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
         <img src={Logo} alt="" />
       </div>
       <div className={styles.title}>
-        <p className={styles.date}>28 Aug 2021 </p>
+        <p className={styles.date}>{today}</p>
         <h1 className={styles.heading}>Today's Time Tables</h1>
       </div>
     </div>
--- src/component/pages/TimeTable/TimeTable.js
+++ src/component/pages/TimeTable/TimeTable.js
@@ -4,7 +4,7 @@ import Days from "./Days";
 import DayTimeLine from "./DayTimeLine";
 import TimeTableOfDay from "./TimeTableOfDay";
 export default function TimeTable() {
-    const curenntDay = new Date()
+  const curenntDay = new Date();
   const timeTable = [
     [
       {
@@ -135,12 +135,28 @@ export default function TimeTable() {
         sub: "maths",
         time: "9-10",
       },
-    ]
+    ],
   ];
+
+  // date
+  var today = new Date();
+  var dd = today.getDate();
+
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  today = dd + "/" + mm + "/" + yyyy;
+
   return (
     <>
       <div className={styles.contentWrapper}>
-        <Header />
+        <Header today={today}/>
         <Days />
         <DayTimeLine />
         <TimeTableOfDay />
