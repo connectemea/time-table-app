b738fe8cf5883e768cb68f19c6729800b35c5247
dilshad <dilshadbasith01@gmail.com>
2021-09-06 13:17:54 +0530

    changes in login page

--- src/component/const/Departments.js
+++ src/component/const/Departments.js
@@ -3,7 +3,37 @@ const Department = [
         name:"Bsc computer science"
     },
     {
-        name:"Micro bioliogy"
-    }
+        name:"Bsc Micro bioliogy"
+    },
+    {
+        name:"Bsc Biotechnology"
+    },
+    {
+        name:"Bsc Biochemistry"
+    },
+    {
+        name:"BA Economics"
+    },
+    {
+        name:"BA English"
+    },
+    {
+        name:"BA West Asian Studies"
+    },
+    {
+        name:"B.Com Computer Application"
+    },
+    {
+        name:"B.Com Cop"
+    },
+    {
+        name:"BBA"
+    },
+    {
+        name:"BVoc Logistics"
+    },
+    {
+        name:"BVoc Professional Accounting and Taxation"
+    },
 ]
 export default Department;
