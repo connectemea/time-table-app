8efc7be7d3d66ca58a6d290a8ca51dbbd3aca24a
dilshad <dilshadbasith01@gmail.com>
2021-09-16 20:06:51 +0530

    was 1st sem

--- src/component/timeTable/was/firstSem.js
+++ src/component/timeTable/was/firstSem.js
@@ -1,7 +1,112 @@
-const mon = [];
-const tue = [];
-const wed = [];
-const thu = [];
-const fri = [];
+const mon = [
+    {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "ENG",
+        teacher: "AFZ",
+      },
+      {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "ENG",
+        teacher: "SJR",
+      },
+      {
+        sub: "WAS",
+        teacher: "SBN",
+      },
+];
+const tue = [
+    {
+        sub: "WAS",
+        teacher: "SBN",
+      },
+      {
+        sub: "ENG",
+        teacher: "AFZ",
+      },
+      {
+        sub: "ENG",
+        teacher: "ABD",
+      },
+      {
+        sub: "LANG",
+        teacher: "",
+      },
+      {
+        sub: "ENG",
+        teacher: "JL",
+      },
+];
+const wed = [
+    {
+        sub: "LANG",
+        teacher: "",
+      },
+      {
+        sub: "ENG",
+        teacher: "SJR",
+      },
+      {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "WAS",
+        teacher: "NZR",
+      },
+      {
+        sub: "WAS",
+        teacher: "SBN",
+      },
+];
+const thu = [
+    {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "ENG",
+        teacher: "ABD",
+      },
+      {
+        sub: "LANG",
+        teacher: "",
+      },
+      {
+        sub: "ENG",
+        teacher: "JL",
+      },
+];
+const fri = [
+    {
+        sub: "ENG",
+        teacher: "ABD",
+      },
+      {
+        sub: "CA",
+        teacher: "SMS",
+      },
+      {
+        sub: "WAS",
+        teacher: "NZR",
+      },
+      {
+        sub: "LANG",
+        teacher: "",
+      },
+      {
+        sub: "WAS",
+        teacher: "NZR",
+      },
+];
 const week = [mon, tue, thu, wed, fri];
 export default week;
