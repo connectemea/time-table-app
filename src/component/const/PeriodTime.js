93e017aa83ff3d376d548a68a559e319b4ca4430
Nahyan <nskonnalath@gmail.com>
2022-01-31 14:10:16 +0530

    Update period time
    * New time updated.

--- src/component/const/PeriodTime.js
+++ src/component/const/PeriodTime.js
@@ -1,16 +1,16 @@
 const common = [
-  "9:00AM - 9:45AM",
-  "9:50AM - 10:30AM",
-  "10:50AM - 11:30AM",
-  "11:40AM - 12:30PM",
-  "12:30PM - 1:15PM",
+  "9:30AM - 10:30AM",
+  "10:30AM - 11:30AM",
+  "11:40AM - 12:40PM",
+  "1:30PM - 2:30PM",
+  "2:30PM - 3:30PM",
 ];
 const special = [
-  "8:45AM - 9:25AM",
-  "9:30AM - 10:10AM",
-  "10:15AM - 10:55AM",
-  "11:00AM - 11:40AM",
-  "11:45AM - 12:15PM"
+   "9:30AM - 10:30AM",
+  "10:30AM - 11:30AM",
+  "11:40AM - 12:40PM",
+  "1:30PM - 2:30PM",
+  "2:30PM - 3:30PM",
 ];
 const periodTime = [common, common, common, common, special]
 export default periodTime;
