f66156f7b6aa2fffaa14673a9618e11d07bc0e63
saneen4 <87607965+saneen4@users.noreply.github.com>
2022-07-20 12:56:31 +0530

    TIME CHANGED

--- src/component/const/PeriodTime.js
+++ src/component/const/PeriodTime.js
@@ -2,8 +2,8 @@ const common = [
   "9:30AM - 10:30AM",
   "10:30AM - 11:30AM",
   "11:40AM - 12:30PM",
-  "1:30PM - 2:30PM",
-  "2:30PM - 3:30PM",
+  "1:30PM - 2:15PM",
+  "2:15PM - 3:00PM",
 ];
 const special = [
    "9:30AM - 10:30AM",
